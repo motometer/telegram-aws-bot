f0e1669bd562cc042374bd007cfa8eda0602241c
Viacheslav Bychkovskyi <vbychkovskyy@gmail.com>
2019-10-26 16:14:39 +0300

    Small refactoring.

--- src/main/java/org/motometer/bot/telegram/publisher/BotHook.java
+++ src/main/java/org/motometer/bot/telegram/publisher/BotHook.java
@@ -1,16 +1,22 @@
 package org.motometer.bot.telegram.publisher;
 
-import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
-import org.motometer.telegram.bot.Bot;
-
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import org.motometer.telegram.bot.Bot;
+import org.motometer.telegram.bot.UpdateListener;
 
 public class BotHook implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
 
-    private final Bot bot = BotConfig.bot();
+    private final Bot bot;
+    private final UpdateListener updateListener;
+
+    public BotHook() {
+        bot = BotConfig.bot();
+        updateListener = new MessageSender(bot);
+    }
 
     @Override
     public APIGatewayProxyResponseEvent handleRequest(final APIGatewayProxyRequestEvent input, final Context context) {
@@ -18,15 +24,17 @@ public class BotHook implements RequestHandler<APIGatewayProxyRequestEvent, APIG
 
         logger.log("Received request with url =" + input.getPath());
 
-        final String body = input.getBody();
+        bot.adaptListener(updateListener)
+            .onEvent(input.getBody());
 
-        bot.adaptListener(new MessageSender(bot))
-            .onEvent(body);
+        logger.log("Request complete");
 
-        logger.log("Successfully handled request");
+        return successResponse();
+    }
 
+    private APIGatewayProxyResponseEvent successResponse() {
         APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent();
-        response.setStatusCode(201);
+        response.setStatusCode(200);
         return response;
     }
 }
