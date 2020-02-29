0715623f83e332c3ac2f010881db21b052e583d8
Viacheslav Bychkovskyi <vbychkovskyy@gmail.com>
2020-02-29 23:07:43 +0200

    Java11 (#8)
    * Migrate to Java 11
    
    * Remove travis CI

--- src/main/java/org/motometer/telegram/bot/aws/BotHook.java
+++ src/main/java/org/motometer/telegram/bot/aws/BotHook.java
@@ -1,7 +1,6 @@
 package org.motometer.telegram.bot.aws;
 
 import com.amazonaws.services.lambda.runtime.Context;
-import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
@@ -15,29 +14,29 @@ import org.motometer.telegram.bot.core.api.WebHookListener;
 
 public class BotHook implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
 
-    private final Collection<WebHookListener> listeners;
+  private final Collection<WebHookListener> listeners;
 
-    public BotHook() {
-        ServiceLoader<WebHookListener> load = ServiceLoader.load(WebHookListener.class);
-        listeners = StreamSupport.stream(load.spliterator(), false).collect(Collectors.toList());
-    }
+  public BotHook() {
+    final var load = ServiceLoader.load(WebHookListener.class);
+    listeners = StreamSupport.stream(load.spliterator(), false).collect(Collectors.toList());
+  }
 
-    @Override
-    public APIGatewayProxyResponseEvent handleRequest(final APIGatewayProxyRequestEvent input, final Context context) {
-        LambdaLogger logger = context.getLogger();
+  @Override
+  public APIGatewayProxyResponseEvent handleRequest(final APIGatewayProxyRequestEvent input, final Context context) {
+    var logger = context.getLogger();
 
-        logger.log("Received request with url =" + input.getPath());
+    logger.log("Received request with url =" + input.getPath());
 
-        listeners.forEach(listener -> listener.onEvent(input.getBody()));
+    listeners.forEach(listener -> listener.onEvent(input.getBody()));
 
-        logger.log("Request complete");
+    logger.log("Request complete");
 
-        return successResponse();
-    }
+    return successResponse();
+  }
 
-    private APIGatewayProxyResponseEvent successResponse() {
-        APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent();
-        response.setStatusCode(200);
-        return response;
-    }
+  private APIGatewayProxyResponseEvent successResponse() {
+    var response = new APIGatewayProxyResponseEvent();
+    response.setStatusCode(200);
+    return response;
+  }
 }
