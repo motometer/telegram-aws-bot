8f39b6d2e274802b05bcfb96ad35d81e6ebbd734
Viacheslav Bychkovskyi <vbychkovskyy@gmail.com>
2020-11-08 18:36:53 +0200

    Add test

--- src/test/java/org/motometer/telegram/bot/aws/service/PostgresRepositoryTest.java
+++ src/test/java/org/motometer/telegram/bot/aws/service/PostgresRepositoryTest.java
@@ -1,5 +1,9 @@
 package org.motometer.telegram.bot.aws.service;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 
 class PostgresRepositoryTest {
@@ -13,5 +17,18 @@ class PostgresRepositoryTest {
     );
 
     postgresRepository.saveUpdate("{\"hello\": \"world\"}");
+  }
+
+  @Test
+  void shouldFindStats() {
+    final PostgresRepository postgresRepository = new PostgresRepository(
+      "jdbc:postgresql://localhost:5432/cube",
+      "cube_user",
+      "cube_user"
+    );
+
+    final List<UserStats> statsByChatId = postgresRepository.findStatsByChatId(-1001211410985L);
+
+    assertThat(statsByChatId).isNotNull();
   }
 }
